cd62288012d66a92d3cc4a947cc61ab72fe46dfb
Andressa Vinci <andressa.vinci@vagas.com.br>
2021-07-15 23:23:11 -0300

    chore: alterando estrutura para fazer CRA funcionar com API em Node com Express

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [response, setResponse] = useState();
+  
+  useEffect(() => {
+    callApi()
+      .then(res => setResponse(res.express))
+      .catch(err => console.log(err));
+  }, []);
+  
+  const callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    
+    return body;
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Response: {response}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
