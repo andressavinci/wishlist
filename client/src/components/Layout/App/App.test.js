3055480d085990a27e91c8ff62a4eaf58dc4094f
Andressa Vinci <andressa.vinci@vagas.com.br>
2021-07-18 01:05:35 -0300

    chore: adicionando configuração para testes com custom render para styled components

--- client/src/components/Layout/App/App.test.js
+++ client/src/components/Layout/App/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { App } from '.';
+import { render, screen } from 'setupTests';
+import { App } from 'components/Layout/App';
 
 test('renders learn react link', () => {
   render(<App />);
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,5 +1,14 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+import React from 'react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import 'jest-styled-components';
+import { Root } from 'components/Layout/Root';
+
+const WrapperProvider = ({ children }) => <Root>{children}</Root>;
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: WrapperProvider, ...options });
+
+export * from '@testing-library/react';
+
+export { customRender as render };
