68606b32f6e01fba0fff25a372d186dcc0625993
Andressa Vinci <andressa.vinci@vagas.com.br>
2021-07-21 23:16:31 -0300

    feat: listando produtos da wishlist na página

--- client/src/components/Layout/PageWishlist/index.js
+++ client/src/components/Layout/PageWishlist/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { object } from 'prop-types';
 import { Header } from 'components/Layout';
-import { Container } from 'components/UI';
+import { Card, Container } from 'components/UI';
+import * as S from './styles';
+import { useLocalStorage } from 'hooks';
 
-const PageWishlist = ({ match }) => {
-  const {
-    params: { productId },
-  } = match;
+const PageWishlist = () => {
+  const isProductAmongWishlist = (arr, id) => arr.some((el) => el.id === id);
+  const [storedWishlistProducts, setStoredWishlistProducts] = useLocalStorage(
+    'mn_wishlist_products',
+    []
+  );
+
+  const toggleWishlistProducts = (product) =>
+    setStoredWishlistProducts((prevState) => {
+      const isWishlistProduct = isProductAmongWishlist(prevState, product.id);
+      const addProduct = prevState.concat(product);
+      const removeProduct = prevState.filter((el) => el.id !== product.id);
+
+      return isWishlistProduct ? removeProduct : addProduct;
+    });
 
   const breadcrumbItems = [
     {
@@ -22,8 +35,26 @@ const PageWishlist = ({ match }) => {
     <>
       <Header breadcrumbItems={breadcrumbItems} />
       <Container as="main">
-        <h1>Lista de desejos</h1>
-        <h2>{productId}</h2>
+        <S.WishlistCardsWrapper>
+          {storedWishlistProducts.length ? (
+            Object.values(storedWishlistProducts).map((obj, index) => (
+              <Card
+                id={obj.id}
+                image={obj.image}
+                isInWishlist={isProductAmongWishlist(storedWishlistProducts, obj.id)}
+                key={index}
+                price={obj.price}
+                title={obj.title}
+                handleAddToWishlistClick={(event) => {
+                  event.preventDefault();
+                  toggleWishlistProducts(obj);
+                }}
+              />
+            ))
+          ) : (
+            <div>Sua lista está vazia :(</div>
+          )}
+        </S.WishlistCardsWrapper>
       </Container>
     </>
   );
